1c40e421ef9d21a40d35f4768a5282226d7d41e8
EMQuintana <ernesto.quintana@inacapmail.cl>
2024-12-16 23:06:56 -0300

    añadir el super usuario en wsgi

--- centro_medico/wsgi.py
+++ centro_medico/wsgi.py
@@ -10,9 +10,23 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth import get_user_model
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'centro_medico.settings')
 
 application = get_wsgi_application()
 
 app= application
+
+
+
+User = get_user_model()
+
+username = "root"  # Cambia esto  # Cambia esto
+password = "root123"  # Cambia esto por una contraseña segura
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, password=password)
+    print("Superusuario creado exitosamente.")
+else:
+    print("El superusuario ya existe.")
